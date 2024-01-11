fcce7d1f585b7428ab192e6007b71b264ff5542c
mehlia <mehliarahman@gmail.com>
2024-01-11 12:05:22 +0000

    skeleton for userservice

--- server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
@@ -1,6 +1,14 @@
 package com.madamepapier.schedulism.services;
 
+import com.madamepapier.schedulism.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-//    test
+
+    @Autowired
+    UserRepository userRepository;
+
 
 }
