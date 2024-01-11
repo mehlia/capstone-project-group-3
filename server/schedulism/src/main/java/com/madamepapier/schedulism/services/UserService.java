c859572d669c0c7d9a765d932679f6ea4537880d
Kacper <kapelek82@outlook.com>
2024-01-11 16:38:37 +0000

    Working on findUserById and exceptions

--- server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
@@ -1,11 +1,15 @@
 package com.madamepapier.schedulism.services;
 
 import com.madamepapier.schedulism.models.User;
+import com.madamepapier.schedulism.models.UserRole;
 import com.madamepapier.schedulism.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.ErrorResponseException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,10 +17,26 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    User user;
+
 //    Display all employees function
     public List<User> findAllUsers(){
         return userRepository.findAll();
     }
 
+    public Optional<User> findUserById(long requesterId, long idToFind) {
+        User requester = userRepository.findById(requesterId).get();
+        if (requester == null){
+            throw new ErrorResponseException(HttpStatus.NOT_FOUND);
+        } else {
+            if (userRepository.findById(requesterId) == userRepository.findById(idToFind)
+                    || requester.getUserRole()== UserRole.HR_EMPLOYEE) {
+                return userRepository.findById(requesterId);
+            } else {
+                throw new ErrorResponseException(HttpStatus.UNAUTHORIZED);
+            }
+        }
+    }
 
 }
