e704cf24256a63fd295578966b0665db7aec5741
Kacper <kapelek82@outlook.com>
2024-01-11 12:16:21 +0000

    creating pojo class for shift_types

--- server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftRotation.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftRotation.java
@@ -1,6 +1,17 @@
 package com.madamepapier.schedulism.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "shift_rotation")
 public class ShiftRotation {
-//    test
+
+    
+    Date date;
+
+
 
 }
--- server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftType.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftType.java
@@ -1,5 +1,58 @@
 package com.madamepapier.schedulism.models;
 
+import jakarta.persistence.*;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "shift_type")
 public class ShiftType {
-//    test
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long shift_type_id;
+
+    @Column
+    private ShiftSlot shiftSlot;
+
+    @Column
+    private Time startTime; // change to DateTime perhaps
+
+    @Column
+    private Time endTime;
+
+
+    public ShiftType(ShiftSlot shiftSlot, Time startTime, Time endTime) {
+        this.shiftSlot = shiftSlot;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public ShiftType(){
+    }
+
+    public ShiftSlot getShiftSlot() {
+        return shiftSlot;
+    }
+
+    public void setShiftSlot(ShiftSlot shiftSlot) {
+        this.shiftSlot = shiftSlot;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+} //Last bracket
