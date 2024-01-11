7281c68abae1ed10a1c5d3b2c48fcb522db1ad3f
mehlia <mehliarahman@gmail.com>
2024-01-11 12:06:41 +0000

    skeleton for usercontroller

--- server/schedulism/src/main/java/com/madamepapier/schedulism/controllers/UserController.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/controllers/UserController.java
@@ -1,5 +1,17 @@
 package com.madamepapier.schedulism.controllers;
 
+import com.madamepapier.schedulism.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
-//    test
+
+    @Autowired
+    UserService userService;
+
+
+
 }
