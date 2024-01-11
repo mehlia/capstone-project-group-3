925358861948ec445ad1d6be5559335f0b1f9f2d
Kacper <kapelek82@outlook.com>
2024-01-11 23:53:40 +0000

    completed findAllShiftsByUserId route but needs testing

--- server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftRotation.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/models/ShiftRotation.java
@@ -1,55 +1,55 @@
-//package com.madamepapier.schedulism.models;
-//
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import jakarta.persistence.*;
-//
-//import java.util.Date;
-//
-//@Entity
-//@Table(name = "shift_rotation")
-//public class ShiftRotation {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column
-//    private Date date;
-//
-//    @Column
-//    private ShiftType shiftType;
-//
-//
-//    public ShiftRotation(Date date, ShiftType shiftType) {
-//        this.date = date;
-//        this.shiftType = shiftType;
-//    }
-//
-//    public ShiftRotation() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Date getDate() {
-//        return date;
-//    }
-//
-//    public void setDate(Date date) {
-//        this.date = date;
-//    }
-//
-//    public ShiftType getShiftType() {
-//        return shiftType;
-//    }
-//
-//    public void setShiftType(ShiftType shiftType) {
-//        this.shiftType = shiftType;
-//    }
-//
-//}
+package com.madamepapier.schedulism.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "shift_rotation")
+public class ShiftRotation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Date date;
+
+    @Column
+    private ShiftType shiftType;
+
+
+    public ShiftRotation(Date date, ShiftType shiftType) {
+        this.date = date;
+        this.shiftType = shiftType;
+    }
+
+    public ShiftRotation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ShiftType getShiftType() {
+        return shiftType;
+    }
+
+    public void setShiftType(ShiftType shiftType) {
+        this.shiftType = shiftType;
+    }
+
+}
--- server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/services/UserService.java
@@ -1,13 +1,16 @@
 package com.madamepapier.schedulism.services;
 
+import com.madamepapier.schedulism.models.ShiftRotation;
 import com.madamepapier.schedulism.models.User;
 import com.madamepapier.schedulism.models.UserRole;
+import com.madamepapier.schedulism.repositories.ShiftRotationRepository;
 import com.madamepapier.schedulism.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.ErrorResponseException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +19,8 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    ShiftRotationRepository shiftRotationRepository;
 
 //    Display all employees function
     public List<User> findAllUsers(){
@@ -48,5 +53,16 @@ public class UserService {
             throw new ErrorResponseException(HttpStatus.UNAUTHORIZED);
         }
         return Optional.of(userToFind);
+    }
+
+    public List<ShiftRotation> findAllShiftsByUserId(long id){
+        List<ShiftRotation> userShifts = new ArrayList<>();
+        for (long x = 0; x<shiftRotationRepository.findAll().size();) {
+            ShiftRotation shiftTemp = shiftRotationRepository.findById(x).get();
+            if (shiftTemp.getUser() == userRepository.findById(id).get()) {
+                userShifts.add(shiftTemp);
+            }
+        }
+        return userShifts;
     }
 }
