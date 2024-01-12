3cb9b7a7d0d88ae8496b5ea1e054655d5d4da30d
mehlia <mehliarahman@gmail.com>
2024-01-12 12:45:18 +0000

    find shift ID in development

--- server/schedulism/src/main/java/com/madamepapier/schedulism/controllers/ShiftRotationController.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/controllers/ShiftRotationController.java
@@ -1,5 +1,33 @@
 package com.madamepapier.schedulism.controllers;
 
+import com.madamepapier.schedulism.models.ShiftRotation;
+import com.madamepapier.schedulism.models.User;
+import com.madamepapier.schedulism.services.ShiftRotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("shift-rotations")
 public class ShiftRotationController {
-//    test
+
+    @Autowired
+    ShiftRotationService shiftRotationService;
+
+    @GetMapping("/{shiftRotationId}/users/{requesterId}")
+    public ResponseEntity<ShiftRotation> findShiftById(
+            @PathVariable long shiftRotationId,
+            @PathVariable long requesterId){
+        try {
+            ShiftRotation foundShift = shiftRotationService.findShiftById(shiftRotationId, requesterId);
+            return new ResponseEntity<>(foundShift, HttpStatus.OK);
+        } catch (ErrorResponseException e) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
 }
--- server/schedulism/src/main/java/com/madamepapier/schedulism/services/ShiftRotationService.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/services/ShiftRotationService.java
@@ -1,9 +1,38 @@
 package com.madamepapier.schedulism.services;
 
+import com.madamepapier.schedulism.models.ShiftRotation;
+import com.madamepapier.schedulism.models.User;
+import com.madamepapier.schedulism.models.UserRole;
+import com.madamepapier.schedulism.repositories.ShiftRotationRepository;
+import com.madamepapier.schedulism.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.ErrorResponseException;
+
+import java.util.Optional;
 
 @Service
 public class ShiftRotationService {
-//    test
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ShiftRotationRepository shiftRotationRepository;
+
+    @Autowired
+    UserService userService;
+
+//   Find shift rotation by ID
+    public ShiftRotation findShiftById(long shiftRotationId, long requesterId){
+        User requester = userRepository.findById(requesterId).orElseThrow(() ->
+                new ErrorResponseException(HttpStatus.NOT_FOUND));
+        ShiftRotation shiftRotationToFind  = shiftRotationRepository.findById(shiftRotationId).orElseThrow(() ->
+                new ErrorResponseException(HttpStatus.NOT_FOUND));
+        if(!(requester.getUserRole() == UserRole.HR_EMPLOYEE) || (requesterId == shiftRotationToFind.getUser().getId())){
+            throw new ErrorResponseException(HttpStatus.FORBIDDEN);
+        }
+        return shiftRotationToFind;
+    }
 }
