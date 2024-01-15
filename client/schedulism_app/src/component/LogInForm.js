58ffac3d5ad211ac0b33bcced92f5e0095c151a7
Rayster Ramos <rjrfrst@gmail.com>
2024-01-15 12:14:17 +0000

    Edited log-in form, still not reloading to userhome when button is clicked

--- client/schedulism_app/src/component/LogInForm.js
+++ client/schedulism_app/src/component/LogInForm.js
@@ -1,16 +1,34 @@
 import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const LogInForm = () => {
 
+    const navigate = useNavigate();
     const [name, setName] = useState("");
-    const [password, setPassword] = useState();
+    const [password, setPassword] = useState("");
+
+
 
     const validateUser = async (password) => {
         try{
-            
+            const response = await fetch(`http://localhost:8080/users/${password}`);
+            const jsonData = await response.json();
+
+            if(name === jsonData.name){
+                // setLogInUser
+                navigate("/user-home");
+            }
+
         } catch (error) {
             throw new Error("Invalid user info")
         }
+    }
+
+    const newUserName = (event) => {
+        setName(event.target.value);
+    }
+    const newUserPassword = (event) => {
+        setPassword(event.target.value);
     }
 
     const handleFormSubmit = (event) => {
@@ -22,6 +40,8 @@ const LogInForm = () => {
                 alert("Incorrect user info");
             });
         }
+
+        
 
         event.preventDefault();
         const newUser = {
@@ -35,17 +55,39 @@ const LogInForm = () => {
 
     }
 
+    
+
 
     return ( 
     <>
     <section className="login-form">
+        <Outlet />
         <section className="login-title">
             <h1>This is the login form</h1>
         </section>
         <div className="form-container" >
             <form className="username-form" onSubmit={(event) => handleFormSubmit(event)}>
-                <input type="text" className="username"/>
-                <input type="text" className="password"/>
+                
+                <label htmlFor="username">Username: </label>
+                <input 
+                    type="text" 
+                    className="username"
+                    onInput={(event) => newUserName(event)}
+                    placeholder="insert username"
+                    value={name}
+                    id = "username"
+                />
+
+                <label htmlFor="password-input">Password: </label>
+                <input 
+                    type="password" 
+                    className="password"
+                    onInput={(event) => newUserPassword(event)}
+                    placeholder="insert password"
+                    value={password}
+                    id = "password-imput"
+                />
+
                 <input type="submit" className="submit" value={"Log-in"}/>
             </form>
         </div>
