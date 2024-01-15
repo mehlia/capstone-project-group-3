40c5173901600c25dbaf567529855280eb49fcc5
mehlia <mehliarahman@gmail.com>
2024-01-15 15:05:32 +0000

    added user to shift & if added to same shift twice wont allow

--- server/schedulism/src/main/java/com/madamepapier/schedulism/repositories/ShiftRotationRepository.java
+++ server/schedulism/src/main/java/com/madamepapier/schedulism/repositories/ShiftRotationRepository.java
@@ -2,13 +2,17 @@ package com.madamepapier.schedulism.repositories;
 
 import com.madamepapier.schedulism.models.ShiftRotation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ShiftRotationRepository extends JpaRepository<ShiftRotation, Long> {
 
     List<ShiftRotation> findAllByHasBeenRequestedTrue();
+
+    List<ShiftRotation> findAllByUserIdAndDate(@Param("userId") long userId, @Param("date") LocalDate date);
 
 }
