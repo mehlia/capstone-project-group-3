317a2b87680ffe95a42d595daa8f0875bbdd9814
Kacper <kapelek82@outlook.com>
2024-01-16 23:53:52 +0000

    Re-adding past commit

--- client/schedulism_app/src/component/Shift.js
+++ client/schedulism_app/src/component/Shift.js
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 
 const Shift = ({shift}) => {
     
-    // console.log(shift.shiftType.id);
+    console.log("Log in Shift.js", shift);  
 
     return ( 
     <div className="shift">
     <h3>Test Shifts</h3>
     <p>date:{shift.date}</p>
     <p>Type of Shift:{shift.id}</p>
-    {/* <p>Start Time: {shift.shiftType.startTime}</p>
-    <p>End Time: {shift.shiftType.endTime}</p> */}
+    {/* <p>Start Time: {shift.shiftType.id}</p>   */}
+    
+    {/* <p>End Time: {shift.shiftType.endTime}</p> */}
     </div> );
     // <Outlet/>
 }
