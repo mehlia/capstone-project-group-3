dd102cbaf81cdff7c365ddaba9ba69ae232b8289
Janet_Mensah <janetmensah60@gmail.com>
2024-01-16 10:19:27 +0000

    Installed Multilevel-sidebar and began creating the condition for rendering the sidebar when it is a employee

--- client/schedulism_app/src/component/NavBar.js
+++ client/schedulism_app/src/component/NavBar.js
@@ -1,4 +1,10 @@
+import MultilevelSidebar from "react-multilevel-sidebar";
+import "react-multilevel-sidebar/src/Sidebar.css";
+import { Sidebar, Menu, MenuItem } from 'react-multilevel-sidebar';
+import {ClientUserContext} from "../containers/PapierContainer";
+
 const NavBar = () => {
+const {globalUser}
     return ( 
         <>
         <h1>This is the nav bar</h1>
