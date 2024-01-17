3754b65b8794ee49badac6890f31a21471050171
mehlia <mehliarahman@gmail.com>
2024-01-17 15:26:51 +0000

    displayed all employees

--- client/schedulism_app/src/component/User.js
+++ client/schedulism_app/src/component/User.js
@@ -5,13 +5,16 @@ import { GlobalUserContext } from "../containers/PapierContainer";
 const User = ({user}) => {
 
   const {globalUser} = useContext(GlobalUserContext);
+
+  const userOne = user ? user : globalUser;
+
     return (
         <div className="user">
         <h3>My Personal Information</h3>
-        <p>Name: {globalUser.name}</p>
-        <p>Email Address: {globalUser.email}</p>
-        <p>Username: {globalUser.username}</p>
-        <p>Occupation: {globalUser.occupation}</p>
+        <p>Name: {userOne.name}</p>
+        <p>Email Address: {userOne.email}</p>
+        <p>Username: {userOne.username}</p>
+        <p>Occupation: {userOne.occupation}</p>
         </div>
       );
 }
