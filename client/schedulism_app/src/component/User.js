edbf7bd3ed337253a82ddcb55cbfe576bca39b09
Kacper <kapelek82@outlook.com>
2024-01-18 13:37:24 +0000

    Re-commit previous changes

--- client/schedulism_app/src/component/User.js
+++ client/schedulism_app/src/component/User.js
@@ -5,49 +5,6 @@ import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-// const User = ({ user, useDelete, useTitle, deleteUserById }) => {
-//   let { globalUser } = useContext(GlobalUserContext);
-
-//   const navigate = useNavigate();
-
-//   const userOne = user ? user : globalUser;
-
-//   const handleDeleteButton = () => {
-//     deleteUserById(globalUser.id, userOne.id);
-
-
-//     if (userOne.id === globalUser.id) {
-//       navigate("/");
-//       globalUser = {
-//         name: "",
-//         id: 0,
-//         email: "",
-//         occupation: "",
-//         userRole: "",
-//         username: "",
-//         shiftRotations: [],
-//       };
-//     } else {
-//       navigate("/user-home");
-//     }
-// };
-
-//   return (
-//     <div className="user">
-//       <h3>Details</h3>
-//       {useTitle ? <h3>Personal info</h3> : null} <p>Name: {userOne.name}</p>
-//       <p>Email Address: {userOne.email}</p>
-//       <p>Username: {userOne.username}</p>
-//       <p>Occupation: {userOne.occupation}</p>
-//       {useDelete ? <button onClick={handleDeleteButton}>Delete</button> : null}
-//     </div>
-//   );
-
-// };
-
-// export default User;
-
-
 const User = ({ user, useDelete, useTitle, deleteUserById }) => {
   const { globalUser } = useContext(GlobalUserContext);
   const navigate = useNavigate();
